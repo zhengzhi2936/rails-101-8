34e8b9be49caee33e11da6039515e53405cd70bc
zhengzhi2936 <zhengzhi2616@gmail.com>
2016-12-29 13:55:31 +0800

    user can login/logout/signup

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,40 +1,47 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user!, only: [:new]
   def index
     @groups = Group.all
   end
+
   def show
     @group = Group.find(params[:id])
   end
+
   def new
     @group = Group.new
   end
+
   def edit
     @group = Group.find(params[:id])
   end
+
   def create
     @group = Group.new(group_params)
-  if @group.save
-    redirect_to groups_path
-  else
-    render :new
-  end
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
 end
+
   def update
     @group = Group.find(params[:id])
 
     if @group.update(group_params)
 
-    redirect_to groups_path, notice: "Update Success"
-  else
-    render :edit
+      redirect_to groups_path, notice: 'Update Success'
+    else
+      render :edit
   end
 end
+
   def destroy
     @group = Group.find(params[:id])
     @group.destroy
-    redirect_to groups_path, alert: "Group deleted"
+    redirect_to groups_path, alert: 'Group deleted'
   end
+
   private
 
   def group_params
