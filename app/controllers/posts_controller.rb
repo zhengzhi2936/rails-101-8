63341666a3177e065a79c292876d19d4fd208882
zhengzhi2936 <zhengzhi2616@gmail.com>
2016-12-29 19:12:11 +0800

    add some new things

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,10 @@ class PostsController < ApplicationController
     @group = Group.find(params[:group_id])
     @post = Post.new
   end
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
 
   def create
     @group = Group.find(params[:group_id])
@@ -16,6 +20,23 @@ class PostsController < ApplicationController
     else
       render :new
     end
+end
+def update
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+  if @post.update(post_params)
+    redirect_to account_posts_path(@group), notice: "Update Success"
+  else
+    render :edit
+  end
+
+end
+def destroy
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to account_posts_path(@group), alert: "post deleted"
+
 end
 
   private
